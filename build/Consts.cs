489de617c8089ac7485e1d469c6c639986d867e2
Soar360 <Soar360@live.com>
2025-04-10 17:22:20 +0800

    v1.9.1

--- build/Consts.cs
+++ build/Consts.cs
@@ -7,5 +7,5 @@ namespace LuYao.TlsClient;
 public static class Consts
 {
     public const string DllName = "tls-client";
-    public static string RuntimeVersion => "v1.7.8";
+    public static string RuntimeVersion => "v1.9.1";
 }
